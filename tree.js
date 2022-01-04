62f9be1bdbf354fe25bf5590191ee4536edc30b2
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2022-01-04 14:00:29 -0800

    Update tree.js

--- tree.js
+++ tree.js
@@ -15,7 +15,22 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
+ 		if(!this.root) return 0;
     
+    let sum = 0;
+    
+    function sumHelp(node){
+    	if(node.children.length === 0){
+      	sum+=node.val;
+      }else{
+      	for(const child of node.children){
+        	sumHelp(child)
+        }
+      }
+    }
+    
+    sumHelp(this.root);
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
